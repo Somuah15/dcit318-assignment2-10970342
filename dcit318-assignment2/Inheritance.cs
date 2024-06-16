0d1e64416ceb7a962fc2b3261b9d0fb51a63ec13
Somuah15 <efsomuah@st.ug.edu.gh>
2024-06-15 17:50:13 -0700

    added codes for the Inheritance.cs

---
+++ dcit318-assignment2/Inheritance.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class InheritanceDemo
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
